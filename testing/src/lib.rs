e6ae4dfb610d2fa779da10a78e757ae3db2c4898
mj <mg.jeong@gmail.com>
2023-12-30 13:36:57 +0900

    add 'testing' samples - part 2

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -1,7 +1,6 @@
 // -------------------------------------------------------------
 // section 01. unit testing
 /*
- */
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
@@ -39,7 +38,7 @@ mod tests {
     }
 
     #[test]
-    fn test_bad_edd() {
+    fn test_bad_add() {
         assert_eq!(bad_add(1, 2), 3);
     }
 
@@ -74,11 +73,73 @@ mod tests {
         divide_non_zero_result(1, 10);
     }
 }
+*/
 
 // -------------------------------------------------------------
 // section 02. documentation testing
 /*
  */
+/// First line is a short summary describing function.
+///
+/// The next lines present detailed documentation. Code blocks start with
+/// triple backquotes and have implicit `fn main()` inside
+/// and `extern crate <cratename>`. Assume we're testing `doccomments` crate:
+///
+/// ```
+/// let result = doccomments::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+/// Usually doc comments may include sections "Examples", "Panics" and "Failures".
+///
+/// The next function divides two numbers.
+///
+/// # Examples
+///
+/// ```
+/// let result = doccomments::div(10, 2);
+/// assert_eq!(result, 5);
+/// ```
+///
+/// # Panics
+///
+/// The function panics if the second argument is zero.
+///
+/// ```rust,should_panic
+/// // panics on division by zero
+/// doccomments::div(10, 0);
+/// ```
+pub fn div(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    }
+
+    a / b
+}
+
+/// Using hidden `try_main` in doc tests.
+///
+/// ```
+/// # // hidden lines start with `#` symbol, but they're still compilable!
+/// # fn try_main() -> Result<(), String> { // line that wraps the body shown in doc
+/// let res = doccomments::try_div(10, 2)?;
+/// # Ok(()) // returning from try_main
+/// # }
+/// # fn main() { // starting main that'll unwrap()
+/// #    try_main().unwrap(); // calling try_main and unwrapping
+/// #                         // so that test will panic in case of error
+/// # }
+/// ```
+pub fn try_div(a: i32, b: i32) -> Result<i32, String> {
+    if b == 0 {
+        Err(String::from("Divide-by-zero"))
+    } else {
+        Ok(a / b)
+    }
+}
 
 // -------------------------------------------------------------
 // section 03. integration testing
@@ -89,5 +150,3 @@ mod tests {
 // section 04. dev-dependencies
 /*
  */
-
-fn main() {}
